6982460d3b63eb9b66129eace12937e2ef5167de
tegra20 <tegkit30@gmail.com>
2024-05-10 21:16:13 +0100

    commit sans trop d'idée de site à faire

---
+++ src/components/home/Index.jsx
@@ -0,0 +1,13 @@
+const Index = () => {
+    return (
+        <>
+            <h1>
+                je suis le commposant pour la carte prrincipale pour afficher du
+                contenu representatif du site
+                
+            </h1>
+        </>
+    );
+};
+
+export default Index;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import Header from '../components/Header.jsx'
+import Header from "../components/Header.jsx";
+import Index from "../components/home/Index.jsx";
 const Home = () => {
     return (
         <>
-        <Header/>
-            <h1> Home</h1>
+            <Header />
+            <Index />
         </>
     );
 };
